ef12246ca86ed7c9e00ffa538016d69e09a76674
James Elner <elner.k.james@gmail.com>
2026-02-25 18:30:44 +0000

    Layout widgets on a screen

--- birdle/lib/main.dart
+++ birdle/lib/main.dart
@@ -10,10 +10,16 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
+        appBar: AppBar(
+          title: Align(
+            alignment: Alignment.centerLeft,
+            child: Text('Birdle'),
+          ),
+        ),
         body: Center(
-          child: Tile('A', HitType.partial),
+          child: GamePage(),
         ),
       ),
     );
@@ -48,4 +54,32 @@ class Tile extends StatelessWidget {
       ),
     );
   }
+}
+
+class GamePage extends StatelessWidget {
+  GamePage({super.key});
+
+  final Game _game = Game();
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        spacing: 5.0,
+        children: [
+          for (var guess in _game.guesses)
+            Row(
+              spacing: 5.0,
+              children: [
+                for (var letter in guess)
+                  Tile(letter.char, letter.type)
+              ],
+            ),
+        ],
+      ),
+    );
+  }
+
+
 }
